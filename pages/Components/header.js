c743d55f63b1533173b71a886db9886b767949a7
sweetyr928 <yeslyn0208@gmail.com>
2023-05-18 22:44:43 +0900

    feat : header 및 navber 구현

---
+++ pages/Components/header.js
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <div className="bg-purple-300 h-24 w-screen flex justify-center items-center">
+      <div className="font-semibold text-4xl text-gray-50">
+        오늘은 무슨 심리테스트를 해볼까?
+      </div>
+    </div>
+  );
+}
---
+++ pages/Components/navbar.js
@@ -0,0 +1,46 @@
+export default function NavBar() {
+  return (
+    <nav className="bg-white py-4">
+      <div className="max-w-7xl mx-auto px-4">
+        <div className="flex justify-around">
+          <a
+            href="#"
+            className="text-purple-300 hover:text-purple-400 px-3 py-2 rounded-md font-medium"
+          >
+            연애
+          </a>
+          <a
+            href="#"
+            className="text-purple-300 hover:text-purple-400 px-3 py-2 rounded-md font-medium"
+          >
+            성향
+          </a>
+          <a
+            href="#"
+            className="text-purple-300 hover:text-text-purple-400 px-3 py-2 rounded-md font-medium"
+          >
+            이상형
+          </a>
+          <a
+            href="#"
+            className="text-purple-300 hover:text-purple-400 px-3 py-2 rounded-md font-medium"
+          >
+            우정
+          </a>
+          <a
+            href="#"
+            className="text-purple-300 hover:text-purple-400 px-3 py-2 rounded-md font-medium"
+          >
+            가치관
+          </a>
+          <a
+            href="#"
+            className="text-purple-300 hover:text-purple-400 px-3 py-2 rounded-md font-medium"
+          >
+            직업
+          </a>
+        </div>
+      </div>
+    </nav>
+  );
+}
