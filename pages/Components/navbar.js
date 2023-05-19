f9d908b7c3ed6aae06d45c1239978c72282199b5
sweetyr928 <yeslyn0208@gmail.com>
2023-05-19 15:19:07 +0900

    refactor : navbar map 사용 방식으로 리팩토링

--- pages/Components/navbar.js
+++ pages/Components/navbar.js
@@ -1,44 +1,18 @@
 export default function NavBar() {
+  const category = ["연애", "성향", "이상형", "우정", "가치관", "직업"];
+
   return (
     <nav className="bg-white py-4">
       <div className="max-w-7xl mx-auto px-4">
         <div className="flex justify-around">
-          <a
-            href="#"
-            className="text-purple-300 hover:text-purple-400 px-3 py-2 rounded-md font-medium"
-          >
-            연애
-          </a>
-          <a
-            href="#"
-            className="text-purple-300 hover:text-purple-400 px-3 py-2 rounded-md font-medium"
-          >
-            성향
-          </a>
-          <a
-            href="#"
-            className="text-purple-300 hover:text-text-purple-400 px-3 py-2 rounded-md font-medium"
-          >
-            이상형
-          </a>
-          <a
-            href="#"
-            className="text-purple-300 hover:text-purple-400 px-3 py-2 rounded-md font-medium"
-          >
-            우정
-          </a>
-          <a
-            href="#"
-            className="text-purple-300 hover:text-purple-400 px-3 py-2 rounded-md font-medium"
-          >
-            가치관
-          </a>
-          <a
-            href="#"
-            className="text-purple-300 hover:text-purple-400 px-3 py-2 rounded-md font-medium"
-          >
-            직업
-          </a>
+          {category.map((el, idx) => (
+            <div
+              className="text-purple-300 hover:text-purple-400 px-3 py-2 rounded-md font-medium"
+              key={idx}
+            >
+              {el}
+            </div>
+          ))}
         </div>
       </div>
     </nav>
