9d490af3ae3b9a1cb9b8fd91435ec225c165ad2c
sweetyr928 <yeslyn0208@gmail.com>
2023-05-20 22:40:30 +0900

    design : TestItem 컴포넌트 height 반응형

--- components/test/testItem.js
+++ components/test/testItem.js
@@ -2,15 +2,21 @@ import Image from "next/image";
 
 export default function TestItem({ id, image, title }) {
   return (
-    <div className="col-span-6 md:col-span-4 lg:col-span-3 aspect-video w-full h-96 bg-purple-200 flex flex-col items-center">
-      <>
-        <Image src={image} alt={title} width={400} height={200} />
-      </>
-      <div className="p-4 flex flex-col items-center flex-grow">
+    <div className="col-span-6 md:col-span-4 lg:col-span-3 w-full bg-purple-200">
+      <div className="aspect-w-2 aspect-h-1">
+        <Image
+          src={image}
+          alt={title}
+          width={400}
+          height={200}
+          className="object-cover"
+        />
+      </div>
+      <div className="p-3 flex flex-col justify-between h-32">
         <div className="text-xl font-semibold mb-2 text-gray-50 line-clamp-2">
           {title}
         </div>
-        <div className="mt-auto">
+        <div className="flex justify-center">
           <button className="bg-purple-400 text-white px-4 py-2 rounded-full">
             테스트 하러가기
           </button>
