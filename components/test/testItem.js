f19a553c8d869102cbe81956a29b59b457694985
sweetyr928 <yeslyn0208@gmail.com>
2023-05-20 22:14:41 +0900

    design : Text Item 버튼 위치 고정(text 길이에 상관없이)

--- components/test/testItem.js
+++ components/test/testItem.js
@@ -2,15 +2,19 @@ import Image from "next/image";
 
 export default function TestItem({ id, image, title }) {
   return (
-    <div className="col-span-6 md:col-span-4 lg:col-span-3 aspect-video w-full bg-purple-200 flex flex-col justify-center content-center">
-      <Image src={image} alt={title} width={400} height={300} />
-      <div className="p-4 flex flex-col justify-center items-center">
-        <h3 className="text-xl font-semibold mb-2 text-gray-50 line-clamp-2">
+    <div className="col-span-6 md:col-span-4 lg:col-span-3 aspect-video w-full h-96 bg-purple-200 flex flex-col items-center">
+      <>
+        <Image src={image} alt={title} width={400} height={200} />
+      </>
+      <div className="p-4 flex flex-col items-center flex-grow">
+        <div className="text-xl font-semibold mb-2 text-gray-50 line-clamp-2">
           {title}
-        </h3>
-        <button className="bg-purple-400 text-white px-4 py-2 rounded-full">
-          테스트 하러가기
-        </button>
+        </div>
+        <div className="mt-auto">
+          <button className="bg-purple-400 text-white px-4 py-2 rounded-full">
+            테스트 하러가기
+          </button>
+        </div>
       </div>
     </div>
   );
