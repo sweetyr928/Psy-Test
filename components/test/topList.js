88e8166b02d66e677e41f7df969c0c9bf5878096
sweetyr928 <yeslyn0208@gmail.com>
2023-06-03 17:31:05 +0900

    design : 컴포넌트 margin bottom 추가

--- components/test/topList.js
+++ components/test/topList.js
@@ -1,13 +1,14 @@
 import { useRouter } from "next/router";
+import { useCallback } from "react";
 
 export default function TopList({ topLists }) {
   const router = useRouter();
-  const handleClick = (id) => {
+  const handleClick = useCallback((id) => {
     router.push(`/${id}`);
-  };
+  }, []);
 
   return (
-    <div className="container mx-auto border-2 border-purple-300 p-4 rounded-lg">
+    <div className="container mx-auto border-2 border-purple-300 p-4 rounded-lg mb-4">
       <div className="text-2xl font-bold mb-4 text-gray-600 cursor-default">
         인기 테스트 TOP 3
       </div>
@@ -22,7 +23,10 @@ export default function TopList({ topLists }) {
           onClick={() => handleClick(el.id)}
         >
           <div className="flex items-center">
-            <div className="flex items-center justify-center w-6 h-6 rounded-full bg-purple-300 text-white text-lg font-semibold mr-2 hover:bg-purple-400">
+            <div
+              className="flex items-center justify-center w-6 h-6 
+            rounded-full bg-purple-300 text-white text-lg font-semibold mr-2 hover:bg-purple-400"
+            >
               {`${idx + 1}`}
             </div>
             <div className="text-lg font-semibold text-gray-500 hover:text-gray-600">
