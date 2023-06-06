447bdd9f8adda8278fe31d6f07412653f5975602
sweetyr928 <yeslyn0208@gmail.com>
2023-06-06 23:10:27 +0900

    design : 헤더 안 텍스트 사이즈 반응형 조절

--- components/layout/header.js
+++ components/layout/header.js
@@ -1,6 +1,24 @@
-import { useCallback } from "react";
+import { useEffect, useState, useCallback } from "react";
 
 export default function Header({ handleReset }) {
+  const [fontSize, setFontSize] = useState(37);
+
+  useEffect(() => {
+    const handleResize = () => {
+      const viewportWidth = window.innerWidth;
+      const calculatedFontSize = Math.min(viewportWidth / 20, 37);
+      setFontSize(calculatedFontSize);
+    };
+
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   const handleClick = useCallback(() => {
     handleReset(true);
   }, []);
@@ -10,8 +28,9 @@ export default function Header({ handleReset }) {
       className="h-24 w-screen flex justify-center items-center cursor-pointer 
     bg-gradient-to-r from-purple-300 via-purple-400 to-purple-300"
       onClick={handleClick}
+      style={{ fontSize: `${fontSize}px` }}
     >
-      <div className="font-semibold text-4xl text-white cursor-pointer">
+      <div className="font-semibold text-white cursor-pointer">
         오늘은 무슨 심리테스트를 해볼까?
       </div>
     </div>
